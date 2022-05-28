799a95e5ad524d079c8cbcc1d3749750de300a49
Luiz Medina <luizmedina87@gmail.com>
2022-05-28 11:42:03 -0400

    initial connection and user table

---
+++ models/User.js
@@ -0,0 +1,8 @@
+const { Model, Datatypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+// Creating the User model
+class User extends Model {
+  checkPassword
+}
