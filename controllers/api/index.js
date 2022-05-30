eaf5733f81f7799c07659e11b19c6cfb9ccb359f
Luiz Medina <luizmedina87@gmail.com>
2022-05-30 19:28:01 -0400

    created answer table and basic APIs

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const userRoutes = require("./user-routes.js");
 const questionRoutes = require("./question-routes.js");
+const answerRoutes = require("./answer-routes.js");
 
 router.use("/users", userRoutes);
 router.use("/questions", questionRoutes);
+router.use("/answers", answerRoutes);
 
 module.exports = router;
