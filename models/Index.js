c5d3ae5d8e511e79e88da2d0bb870aa9b6a2bd64
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-05-31 19:59:57 -0400

    question answer and user associations defined

--- models/Index.js
+++ models/Index.js
@@ -3,4 +3,20 @@ const Question = require("./Question");
 const Answer = require("./Answer");
 const Match = require("./Match");
 
+User.hasMany(Question, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Answer, {
+    foreignKey: 'user_id'
+});
+
+Question.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Question.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Question, Answer, Match };
