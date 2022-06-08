6f890a5d5cbbc504f0ac724e2443bfa3d3ba00b4
Oindrila Talukder <oindrilatalukder.ot@gmail.com>
2022-06-08 18:38:10 -0500

    fixed route issue

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,24 +1,11 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+//const { User } = require('../models');
  router.get('/', (req, res) => {
-     console.log(req.session);
-    User.findAll({
-      attributes: [
-        'id',
-        'email',
-        
-      ],
-
-    })
-      .then(dbUserInfo => {
-       const users = dbUserInfo.map(user => user.get({plain:true}));
-        res.render('homepage', {users} );
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+  
+        res.render('homepage' );
+      
+     
   });
 
   router.get('/login', (req, res) => {
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const sess = {
